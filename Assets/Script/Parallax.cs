4f9b0365c4944fc5cb25b1905e51513d60f50b30
Tien The President <tientran.05062006@gmail.com>
2026-07-11 18:54:02 +0700

    let beggin

--- Assets/Script/Parallax.cs
+++ Assets/Script/Parallax.cs
@@ -2,26 +2,37 @@ using UnityEngine;
 
 public class Parralax : MonoBehaviour
 {
+    #region parallaxRightToLeft;
     [SerializeField] float depth = 1f;
-    Player player;
+    public Player player;
+    public float limitDistance = -30f;
+    public float newDistance = 80f;
+
     private void Awake()
     {
         player = GameObject.Find("Player").GetComponent<Player>();
     }
 
-    private void FixedUpdate()
+    // Đổi từ FixedUpdate sang LateUpdate để tránh giật hình
+    private void LateUpdate()
     {
+        // Bây giờ player.velocity.x đã có giá trị lớn hơn 0
         float realVelocity = player.velocity.x / depth;
 
         Vector2 pos = transform.position;
 
-        pos.x -= realVelocity * Time.fixedDeltaTime;
+        // Dùng Time.deltaTime thay vì fixedDeltaTime vì ta đang ở LateUpdate
+        pos.x -= realVelocity * Time.deltaTime;
 
-        if( pos.x <= -50)
+        if (pos.x <= limitDistance)
         {
-            pos.x = 50;
+            pos.x = newDistance;
         }
 
         transform.position = pos;
     }
+    #endregion
+
+
+
 }
